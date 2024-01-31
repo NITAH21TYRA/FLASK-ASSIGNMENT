583e98c4bfe8f1b0e39352ba28232b36fd84b4d2
NANSALIRE_ANITAH <anitanansa@gmail.com>
2024-01-31 12:52:57 +0300

    Flask Assignment

--- assignment_env/app.py
+++ assignment_env/app.py
@@ -15,22 +15,18 @@ def contact():
     return render_template('contact.html')
 
 @app.route('/user/<username>')
-def user(username):  
-    return f'Hello,{username}'
-@app.route('/submit', methods=['GET', 'POST'])
-
+def greet_user(username):  
+    return render_template('user.html', username=username)
 
 @app.route('/submit', methods=['GET','POST'])
 def submit():  
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        return f'Thank you, {name}! We received you.'
-    return render_template('form.html')
+        return render_template('thank_you.html',name=name, email=email)
+    return render_template('submit.html')
 
 
-@app.route('/hello', methods=['GET','POST'])
-def index():
-    if request.method =='POST':
-        return 'This is a request'
-    return render_template('home.html')
+@app.route('/thank_you')
+def thank_you():
+    return render_template('submit.html')
